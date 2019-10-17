efc6e7abd979cee3221f59851335cfcf2676fb1f
Henry Rocha <henryrocha05@hotmail.com>
2019-10-17 00:20:35 -0300

    Busca de filmes implementada

--- Back/MoviesAPI/movies.js
+++ Back/MoviesAPI/movies.js
@@ -4,13 +4,15 @@ const util = require('util');
 const axios = require('axios');
 const mongo = require('mongodb');
 
-const {MongoClient} = mongo;
+const { MongoClient } = mongo;
+
 const CONFIGS = {
   dbUrl: 'mongodb+srv://Henrer:Rocher@techweb-r9i58.mongodb.net/admin?retryWrites=true&w=majority',
   // dbUrl: 'mongodb://127.0.0.1:27017',
   dbName: 'movies',
   dbAuthName: 'TechWeb',
 };
+
 const uflixit = {
   baseUrl: 'https://uflixit.p.rapidapi.com',
   headers: {
@@ -20,22 +22,26 @@ const uflixit = {
   },
 };
 
+const theMovieDB = {
+  searchByTitleURL: "https://api.themoviedb.org/3/search/movie?api_key=e065aad1be38f0202d339b9fe5533ef9&query="
+}
+
 app.use(parser.json());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods',
-      'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers',
-      'X-Requested-With,content-type');
+    'X-Requested-With,content-type');
 
   next();
 });
 
 const client = new MongoClient(CONFIGS.dbUrl,
-    {useNewUrlParser: true, useUnifiedTopology: true});
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect((err) => {
   if (err) {
@@ -53,138 +59,162 @@ client.connect((err) => {
   async function authenticateUser(userId, token) {
     let tokenId;
     await client.db(CONFIGS.dbAuthName).collection('tokens')
-        .findOne({token: token})
-        .then((v) => {
-          tokenId = v.userId;
-        })
-        .catch((e) => {
-          tokenId = null;
-        });
+      .findOne({ token: token })
+      .then((v) => {
+        tokenId = v.userId;
+      })
+      .catch((e) => {
+        tokenId = null;
+      });
     return userId === tokenId;
   }
 
   app.route('/movies')
-      .post(async (req, res, next) => {
-        const category = 'movies';
-        const type = req.body.type;
+    .post(async (req, res, next) => {
+      const category = 'movies';
+      const type = req.body.type;
 
-        let movies;
-        await axios({
-          'method': 'GET',
-          'url': util.format('%s/%s/%s', uflixit.baseUrl, category, type),
-          'headers': uflixit.headers,
-        }).then((v)=>{
-          movies = v.data.result;
-        }).catch((e)=>{
-          movies = null;
-        });
-        if (movies) {
-          const payload = {
-            type: type,
-            movies: movies,
-          };
-          await db.collection(category).insertOne(payload).then((v) => {
-            res.send({
-              [category]: movies,
-            });
-          }).catch((e) => {
-            res.status(400).send({
-              message: e.message,
-            });
-          });
-        } else {
-          res.status(400).send({
-            message: 'Deu Ruim Ze',
-          });
-        }
-      })
-      .get(async (req, res, next) => {
-        const category = 'movie';
-        const type = req.query.type;
-        let dbMovies;
-        await db.collection(category).find({type: type}).toArray().then((v) => {
-          dbMovies = v;
+      let movies;
+      await axios({
+        'method': 'GET',
+        'url': util.format('%s/%s/%s', uflixit.baseUrl, category, type),
+        'headers': uflixit.headers,
+      }).then((v) => {
+        movies = v.data.result;
+      }).catch((e) => {
+        movies = null;
+      });
+      if (movies) {
+        const payload = {
+          type: type,
+          movies: movies,
+        };
+        await db.collection(category).insertOne(payload).then((v) => {
           res.send({
-            movies: dbMovies,
+            [category]: movies,
           });
         }).catch((e) => {
           res.status(400).send({
             message: e.message,
           });
         });
+      } else {
+        res.status(400).send({
+          message: 'Deu Ruim Ze',
+        });
+      }
+    })
+    .get(async (req, res, next) => {
+      const category = 'movie';
+      const type = req.query.type;
+      let dbMovies;
+      await db.collection(category).find({ type: type }).toArray().then((v) => {
+        dbMovies = v;
+        res.send({
+          movies: dbMovies,
+        });
+      }).catch((e) => {
+        res.status(400).send({
+          message: e.message,
+        });
       });
+    });
 
   app.route('/movie/:movieId')
-      .post(async (req, res, next) => {
-        const category = 'movies';
-        const type = req.body.type;
-        let movie;
+    .post(async (req, res, next) => {
+      const category = 'movies';
+      const type = req.body.type;
+      let movie;
 
-        await axios({
-          'method': 'GET',
-          'url': util.format('%s/%s/details/%s',
-              uflixit.baseUrl,
-              category,
-              req.params.movieId),
-          'headers': uflixit.headers,
-        }).then((v) => {
-          movie = v.data.result;
+      await axios({
+        'method': 'GET',
+        'url': util.format('%s/%s/details/%s',
+          uflixit.baseUrl,
+          category,
+          req.params.movieId),
+        'headers': uflixit.headers,
+      }).then((v) => {
+        movie = v.data.result;
+      }).catch((e) => {
+        movie = null;
+      });
+      if (movie) {
+        movie.imdbId = movie.imdb_id;
+        movie.type = type;
+        delete movie.imdb_id;
+        await db.collection(category).insertOne(movie).then((v) => {
+          movie.id = v.insertedId;
+          res.send(movie);
         }).catch((e) => {
-          movie = null;
-        });
-        if (movie) {
-          movie.imdbId = movie.imdb_id;
-          movie.type = type;
-          delete movie.imdb_id;
-          await db.collection(category).insertOne(movie).then((v) => {
-            movie.id = v.insertedId;
-            res.send(movie);
-          }).catch((e) => {
-            res.status(400).send({
-              message: e.message,
-            });
+          res.status(400).send({
+            message: e.message,
           });
+        });
+      } else {
+        res.status(400).send({
+          message: 'Deu Ruim Ze',
+        });
+      };
+    })
+    .get(async (req, res, next) => {
+      const category = 'movie';
+      const movieId = req.params.movieId;
+
+      await db.collection(category).findOne({ imdbId: movieId }).then((v) => {
+        if (v) {
+          const movie = v;
+          movie.id = movie._id;
+          delete movie._id;
+          res.send(movie);
         } else {
           res.status(400).send({
-            message: 'Deu Ruim Ze',
+            message: 'No movie with refered id',
           });
-        };
-      })
-      .get(async (req, res, next) => {
-        const category = 'movie';
-        const movieId = req.params.movieId;
-
-        await db.collection(category).findOne({imdbId: movieId}).then((v) => {
-          if (v) {
-            const movie = v;
-            movie.id = movie._id;
-            delete movie._id;
-            res.send(movie);
-          } else {
-            res.status(400).send({
-              message: 'No movie with refered id',
-            });
-          }
-        }).catch((e) => {
-          res.status(400).send({
-            message: e.message,
-          });
-        });
-      })
-      .patch(async (req, res, next) => {
-        const category = 'movie';
-        const movieId = req.params.movieId;
-
-        await db.collection(category).updateOne({imdbId: movieId}, {
-          $set: req.body,
-        }).then((v) => {
-          res.send(v);
-        }).catch((e) => {
-          res.status(400).send({
-            message: e.message,
-          });
+        }
+      }).catch((e) => {
+        res.status(400).send({
+          message: e.message,
         });
       });
+    })
+    .patch(async (req, res, next) => {
+      const category = 'movie';
+      const movieId = req.params.movieId;
+
+      await db.collection(category).updateOne({ imdbId: movieId }, {
+        $set: req.body,
+      }).then((v) => {
+        res.send(v);
+      }).catch((e) => {
+        res.status(400).send({
+          message: e.message,
+        });
+      });
+    });
+
+  // GET movie by title
+  app.route("/search").get(async (req, res, next) => {
+    console.log("GET em /search:")
+
+    const title = req.query.t;
+
+    console.log("Movie title: " + title);
+    console.log("Fazendo request no TheMovieDB...")
+
+    await axios.get(theMovieDB.searchByTitleURL + title)
+      .then((v) => {
+        console.log("Resposta:");
+
+        console.log(v.data);
+
+        res.send(v.data);
+      })
+      .catch((e) => {
+        res.status(400).send({
+          message: e.message,
+        });
+      });
+  });
 });
 
 app.listen(8081);
--- app/src/Screens/SearchScreen.js
+++ app/src/Screens/SearchScreen.js
@@ -1,19 +1,99 @@
-import React, {Component} from 'react';
-import {Spinner} from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Spinner, Button } from 'react-bootstrap';
 import axios from 'axios';
 import Header from '../Components/Header';
 import SideBar from '../Components/SideBar';
-import {Colors} from '../Config/Colors';
+import { Colors } from '../Config/Colors';
 
 export class SearchScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      movieTitle: "",
+      searchResults: {
+        results: []
+      },
+
+      searchLoading: false
+    }
+
     this.user = JSON.parse(localStorage.getItem('user'));
+
+    // Connecting the methods
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    // Getting the component that changed.
+    const target = event.target;
+
+    // Getting the name of the component that changed.
+    const name = target.name;
+
+    // Getting the new value.
+    const value = target.value;
+
+    // Updating the state according to the component that changed.
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      searchLoading: true
+    });
+
+    const movieTitle = this.state.movieTitle;
+
+    console.log("Buscando por: " + movieTitle);
+
+    axios({
+      "method": "GET",
+      "url": "http://localhost:8081/search?",
+      "params": {
+        "t": movieTitle,
+        "y": ""
+      }
+    })
+      .then((v) => {
+        var data = v.data;
+        console.log(data);
+
+        this.setState({
+          searchResults: data
+        });
+
+        console.log("Sucesso.");
+      })
+      .catch((v) => {
+        console.log(v.data);
+        console.log("Erro ao procurar o filme.")
+      });
+
+    this.setState({
+      searchLoading: false
+    });
+
+    event.preventDefault();
   }
 
   render() {
     if (this.user) {
       if (this.user.valid) {
+
+        const results = this.state.searchResults.results.map((result) =>
+          result.result.map((movie) =>
+            <div>
+              <li>{movie.title}</li>
+              <li>{movie.release_date}</li>
+              <li>{movie.overview}</li>
+            </div>
+          )
+        );
+
         return (
           <div>
             <Header />
@@ -26,12 +106,37 @@ export class SearchScreen extends Component {
             }}>
               <SideBar />
             </div>
+
+            <ul>{results}</ul>
           </div>
         );
       } else {
+
+        const results = this.state.searchResults.results.map((movie) =>
+          <div>
+            <li>{movie.title}</li>
+            <li>{movie.release_date}</li>
+            <li>{movie.overview}</li>
+          </div>
+        );
+
+        const submitButton =
+            <Button type="submit" value="Submit" >
+              Submit
+            </Button>;
+
+        if (this.state.searchLoading === true) {
+          const submitButton =
+            <Button type="submit" value="Submit" >
+              Submit
+              <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+            </Button>;
+        }
+
         return (
           <div>
             <Header />
+
             <div style={{
               width: '100%',
               heigth: 'auto',
@@ -39,7 +144,8 @@ export class SearchScreen extends Component {
               display: 'flex',
               background: Colors.background,
             }}>
-              <SideBar history={this.props.history}/>
+              <SideBar history={this.props.history} />
+
               <div style={{
                 width: 'auto',
                 maxWidth: '50%',
@@ -50,7 +156,20 @@ export class SearchScreen extends Component {
                 color: Colors.greyText,
                 alignItems: 'center',
                 textAlign: 'center',
-              }}><h3 style={{margin: 'none'}}>Feature can only be used by users who validated their phone number. Please validate your phone number on your profile!</h3></div>
+              }}>
+                <h3 style={{ margin: 'none' }}>Feature can only be used by users who validated their phone number. Please validate your phone number on your profile!</h3>
+
+                <form onSubmit={this.handleSubmit}>
+                  <label>
+                    Search:
+                    <input type="text" value={this.state.movieTitle} onChange={this.handleChange} name="movieTitle" placeholder="Movie Title" />
+                  </label>
+
+                  {submitButton}
+                </form>
+
+                <ul>{results}</ul>
+              </div>
             </div>
           </div>
         );
